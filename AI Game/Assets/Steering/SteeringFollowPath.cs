aeae2351437db89c300b021f56440533bb6858c2
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2020-10-26 21:20:40 +0100

    shitposting

--- AI Game/Assets/Steering/SteeringFollowPath.cs
+++ AI Game/Assets/Steering/SteeringFollowPath.cs
@@ -19,33 +19,70 @@ public class SteeringFollowPath : MonoBehaviour {
     private BGCcMath bgMath;
     private BGCurveBaseMath.SectionInfo section;
     private float near_dest;
+
+
+
+    private Vector3 distance;
+    private float tdist =0;
+    public GameObject test;
+
     void Start () {
 		move = GetComponent<Move>();
 		seek = GetComponent<SteeringSeek>();
         curve = Path.GetComponent<BGCurve>();
         bgMath = Path.GetComponent<BGCcMath>();
         // TODO 1: Calculate the closest point from the tank to the curve
-        section = Path.GetComponent<BGCurveBaseMath.SectionInfo>();
-        //bgMath.sec
-        //var n_dst = bgMath.CalcByDistance()
+      //  section = Path.GetComponent<BGCurveBaseMath.SectionInfo>();
+
+
+        int nearPoint = -1;
+        var points = curve.Points;
+        var pointsCount = points.Length;
+      
+       // Debug.Log(section);
+        float shortestpoint =-1;
+
+        // Get Shortest point of the curve & distance
+        for (int i = 0; i < pointsCount; i++)
+        {
+            Debug.Log(points[i]);
+            Vector3 pp =test.transform.position - points[i].PositionWorld;
+
+
+            if(pp.magnitude < shortestpoint && shortestpoint != -1)
+            {
+                shortestpoint = pp.magnitude;
+                nearPoint = i;
+                Debug.Log(shortestpoint);
+                Debug.Log(nearPoint);
+            }
+            //points[i];
+
+        }
+        //cada seccio té 15 parts
+        int parts = bgMath.SectionParts;
+        Debug.Log(parts);
+        Vector3 cdis = points[0].PointTransform.position;
+        distance = test.transform.position - cdis;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-        var points = curve.Points;
-        var pointsCount = points.Length;
-        Debug.Log(section);
-        for(int i =0; i < pointsCount; i++)
-        {
-            Debug.Log(points[i]);
-            //points[i];
-            
-        }
+       
+        //var points = curve.Points;
+        //var pointsCount = points.Length;
+        //Debug.Log(section);
+        //for(int i =0; i < pointsCount; i++)
+        //{
+        //    Debug.Log(points[i]);
+        //    //points[i];
 
-		// TODO 2: Check if the tank is close enough to the desired point
-		// If so, create a new point further ahead in the path
-	}
+        //}
+
+        // TODO 2: Check if the tank is close enough to the desired point
+        // If so, create a new point further ahead in the path
+    }
 
  
 	void OnDrawGizmosSelected() 
