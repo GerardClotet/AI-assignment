f0bfe45082c41539a8652d071b63c7c40c55e790
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2020-10-27 14:53:59 +0100

    enemies turrets point each other

--- AI Game/Assets/Scripts/Steering/SteeringFollowPath.cs
+++ AI Game/Assets/Scripts/Steering/SteeringFollowPath.cs
@@ -8,22 +8,17 @@ public class SteeringFollowPath : MonoBehaviour {
 	Move move;
 	SteeringSeek seek;
 
-    public float ratio_increment = 0.1f;
-    public float min_distance = 1.0f;
-    float current_ratio = 0.0f;
+    //public float ratio_increment = 0.1f;
+    //public float min_distance = 1.0f;
+    //float current_ratio = 0.0f;
 
     public GameObject Path;
-    // Use this for initialization
-    //private
-    private BGCurve curve;
     private BGCcMath bgMath;
-    private BGCurveBaseMath.SectionInfo section;
     private float near_dest;
 
 
 
     private float distance;
-    private float tdist =0;
     public GameObject test;
 
 
@@ -35,7 +30,6 @@ public class SteeringFollowPath : MonoBehaviour {
         calctype = CALCTYPE.CLOSESTPOINT;
 		move = GetComponent<Move>();
 		seek = GetComponent<SteeringSeek>();
-        curve = Path.GetComponent<BGCurve>();
         bgMath = Path.GetComponent<BGCcMath>();
         // TODO 1: Calculate the closest point from the tank to the curve
       //  section = Path.GetComponent<BGCurveBaseMath.SectionInfo>();
@@ -81,7 +75,7 @@ public class SteeringFollowPath : MonoBehaviour {
         Vector3 direction = startRot_Pos[0] - test.transform.position;
         test.transform.rotation = Quaternion.LookRotation(startRot_Pos[1]);
 
-        test.transform.position += direction.normalized * 5 * Time.deltaTime;
+        test.transform.position += direction.normalized * 6 * Time.deltaTime;
 
         if (Mathf.Abs((startRot_Pos[0] - test.transform.position).magnitude) <= 0.1f)
             calctype = CALCTYPE.FOLLOWPATH;
@@ -95,7 +89,7 @@ public class SteeringFollowPath : MonoBehaviour {
         Vector3 tangent;
         test.transform.position = bgMath.CalcPositionAndTangentByDistance(distance, out tangent);
         test.transform.rotation = Quaternion.LookRotation(tangent);
-        distance += 5f * Time.deltaTime;
+        distance += 6f * Time.deltaTime;
     }
 	void OnDrawGizmosSelected() 
 	{
