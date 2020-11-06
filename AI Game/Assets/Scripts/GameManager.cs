2e6014883f4e6853bd1fb66d8dc231dc58619d28
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2020-11-06 14:19:22 +0100

    ui circular healthbar on world pos working

--- AI Game/Assets/Scripts/GameManager.cs
+++ AI Game/Assets/Scripts/GameManager.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    //public GameObject redTank;
-    //public GameObject blueTank;
+    public GameObject redTank;
+    public GameObject blueTank;
     private float redTankLife = 100f;
     private float blueTankLife = 100f;
+  
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -21,14 +22,22 @@ public class GameManager : MonoBehaviour
         
     }
 
+    private void SetHealthSlider(float amount)
+    {
+        // redTank.byname
+    }
 
     public void DecreaseRedTankLife(float amount)
     {
         redTankLife -= amount;
+        redTank.GetComponentInChildren<Slider>().value = redTankLife;
+
     }
     public void DecreaseBlueTankLife(float amount)
     {
         blueTankLife -= amount;
+        blueTank.GetComponentInChildren<Slider>().value = redTankLife;
+
     }
 
 }
