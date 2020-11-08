058ff484b70ff206993799babc1089328b0aace6
AlexandruC5 <36231161+AlexandruC5@users.noreply.github.com>
2020-11-08 18:48:03 +0100

    fully implemented AudioManager

--- AI Game/Assets/AudioManager.cs
+++ AI Game/Assets/AudioManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 
@@ -10,14 +11,22 @@ public class AudioManager : MonoBehaviour
     public Sound[] sounds;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Play(string name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
     }
 }
